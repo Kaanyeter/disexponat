f6ac0c94bf25221ec4c89d88e13a1d1eee3b8060
Kaanyeter <senorkaan@hotmail.com>
2024-02-20 10:52:57 +0100

    combined backend and finished interface v1

--- Prototype.py
+++ Prototype.py
@@ -1,27 +1,54 @@
 import tkinter as tk
-from backend import capture_image
-from tkinter_webcam import webcam
-from time import sleep
-import cv2
 import numpy as np
 from PIL import Image, ImageTk
+from transformers import DetrImageProcessor, DetrForObjectDetection
+import torch
+import cv2
 
+def capture_image():
+        vidcap = cv2.VideoCapture(0)
+
+        success, frame = vidcap.read()
+
+        cv2.imshow("Webcam", frame)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+
+        # you can specify the revision tag if you don't want the timm dependency
+        processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+        model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50", revision="no_timm")
+
+        inputs = processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+
+        # convert outputs (bounding boxes and class logits) to COCO API
+        # let's only keep detections with score > 0.9
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+        output = ""
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+                box = [round(i, 2) for i in box.tolist()]
+                output += f"Detected {model.config.id2label[label.item()]} with confidence {round(score.item(), 3)} at location {box}"
+        if output == "":
+            output = "Ich konnte keine Objekte erkennen :("
+        displaytext.set(output)
+    
+
+placeholder = "Nehmen sie ein Bild auf um die KI zu testen :)"
 root = tk.Tk()
 root.geometry("1080x720")
 root.configure(bg="black")
-label = tk.Label(root, text="AI Visualization", font=("times new roman", 20, "bold"))
-f1 = tk.LabelFrame(root, bg="red")
-f1.pack()
-L1 = tk.Label(f1,bg="red")
-L1.pack()
-stream = cv2.VideoCapture(0)
-Button = tk.Button(root, text="Capture a Picture", command=capture_image())
-Button.pack()
+root.title("KI Bilderkennung")
+displaytext = tk.StringVar()
+displaytext.set(placeholder)
 
-while True:
-
-        img = stream.read()[1]
-        img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        img = ImageTk.PhotoImage(Image.fromarray(img))
-        L1["image"] = img
-        root.update()
+label = tk.Label(root, text="KI Bilderkennung", font=("Arial", 20), bg="black", fg="white")
+label.pack(pady=20)
+out = tk.Label(root, textvariable=displaytext, font=("Arial", 20), bg="black", fg="white")
+out.pack(pady=20)
+Capturebutton = tk.Button(root, text="Capture a Picture", command=lambda:capture_image())
+Capturebutton.pack(padx=10, pady=100)
+root.mainloop()
